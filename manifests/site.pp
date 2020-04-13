d3d10f89e627410128fedd59440105a6f2d4bb60
kienmorz <43597280+kienmorz@users.noreply.github.com>
2020-04-13 15:14:33 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,11 @@ node 'master.puppet.vm' {
   include role::master_server
   include profile::agent_nodes
 }
+
+node /^web.*/ {
+  include role::app_server
+}
+
+node /^db.*/ {
+  include role::db_server
+}
